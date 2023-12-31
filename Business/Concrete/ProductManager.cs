eb78af177667c82b616796e64e923d74f43ecf21
Ko1suz <rooooocer@gmail.com>
2023-12-31 20:06:06 +0300

    Update
    Result Sınıfları

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -1,4 +1,5 @@
 ﻿using Business.Abstract;
+using Core.Utilities.Results;
 using DataAccess.Abstract;
 using DataAccess.Concrete.InMemory;
 using Entities.Concrete;
@@ -20,6 +21,16 @@ namespace Business.Concrete
             _productDal = productDal;
         }
 
+        public IResult Add(Product product)
+        {
+            _productDal.Add(product);
+            if (product.ProductName.Length < 2)
+            {
+                return new ErrorResult("Ürün ismi en az iki karakter olmalıdır");
+            }
+            return new SuccessResult("Ürün Eklendi -> " + product.ProductName);
+        }
+
         public List<Product> GetAll()
         {
             // İş kodları
@@ -29,6 +40,11 @@ namespace Business.Concrete
         public List<Product> GetAllByCategoryId(int id)
         {
             return _productDal.GetAll(p => p.CategoryID == id);
+        }
+
+        public Product GetByID(int productID)
+        {
+            return _productDal.Get(p => p.ProductID == productID);
         }
 
         public List<Product> GetByPrice(decimal min, decimal max)
