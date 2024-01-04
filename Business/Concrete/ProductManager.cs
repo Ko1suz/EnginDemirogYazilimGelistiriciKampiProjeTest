247c13f5514d8aaa139cf6350cc83c7b1319c2e6
Ko1suz <rooooocer@gmail.com>
2024-01-04 11:39:26 +0300

    Update
    Validation Tool yazılıyor yarıda bıraktım
    Videoya 01:56:49 süresinden devam et

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -1,15 +1,20 @@
 ﻿using Business.Abstract;
 using Business.Constants;
+using Business.ValidationRules.FluentValidation;
+using Core.CrossCuttingConcerns.Validation;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using DataAccess.Concrete.InMemory;
 using Entities.Concrete;
 using Entities.DTOs;
+using FluentValidation;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ValidationException = FluentValidation.ValidationException;
 
 namespace Business.Concrete
 {
@@ -27,16 +32,9 @@ namespace Business.Concrete
             //Business Codes
             //validaton - doğrulama
 
-            if (product.Price <= 0)
-            {
-                return new ErrorResult(Messages.UnitPriceInvalid);
-            }
+            ValidationTool.Validate(new ProductValidator(), product);
 
             _productDal.Add(product);
-            if (product.ProductName.Length < 2)
-            {
-                return new ErrorResult(Messages.ProductNameInvalid);
-            }
             return new SuccessResult(Messages.ProductAdded + product.ProductName);
         }
 
---
+++ Business/ValidationRules/FluentValidation/ProductValidator.cs
@@ -0,0 +1,29 @@
+﻿using Entities.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.ValidationRules.FluentValidation
+{
+    public class ProductValidator : AbstractValidator<Product>
+    {
+        public ProductValidator()
+        {
+            RuleFor(p => p.ProductName).NotEmpty();
+            RuleFor(p => p.ProductName).MinimumLength(2);
+            RuleFor(p => p.Price).NotEmpty();
+            RuleFor(p => p.Price).GreaterThan(0);
+            RuleFor(p => p.Price).GreaterThan(10).When(p => p.CategoryID == 1);
+            RuleFor(p => p.ProductName).Must(StartWithA).WithMessage("Ürünler A harfi ile başlamalıdır...");
+
+        }
+
+        bool StartWithA(string args)
+        {
+            return args.StartsWith("A");
+        }
+    }
+}
