eefc26baa4956e8b77b4e9cf281f3ac029342e90
Ko1suz <rooooocer@gmail.com>
2024-01-05 06:33:45 +0300

    Update
    Ders12
    Validaton Tool

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -1,6 +1,7 @@
 ﻿using Business.Abstract;
 using Business.Constants;
 using Business.ValidationRules.FluentValidation;
+using Core.Aspects.Autofac.Validation;
 using Core.CrossCuttingConcerns.Validation;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
@@ -27,13 +28,9 @@ namespace Business.Concrete
             _productDal = productDal;
         }
 
+        [ValidationAspect(typeof(ProductValidator))]
         public IResult Add(Product product)
         {
-            //Business Codes
-            //validaton - doğrulama
-
-            ValidationTool.Validate(new ProductValidator(), product);
-
             _productDal.Add(product);
             return new SuccessResult(Messages.ProductAdded + product.ProductName);
         }
